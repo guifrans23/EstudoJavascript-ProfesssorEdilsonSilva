c2f47532094ea0e616d95dff9825aa34c505cdb7
EDILSON JESUS DA SILVA <edilson.jsilva7@senacsp.edu.br>
2022-05-02 15:28:52 -0300

    alteracao

--- 9-28042022/index.js
+++ 9-28042022/index.js
@@ -23,11 +23,17 @@ const express = require("express");
 //importação do modulo do mongoose
 const mongoose = require("mongoose");
 
+//importação do modulo do cors
+const cors = require("cors");
+
 //criação do app referente ao express
 const app = express();
 
 //preparar o servidor para receber json
 app.use(express.json());
+
+//Uso do cors para desbloqueio de acesso
+app.use(cors());
 
 /*
 caminho do banco de dados mongodb
@@ -55,7 +61,16 @@ const default_route = "/api/cliente";
 
 //rota para listar os clientes com endpoint listar
 app.get(`${default_route}/listar`, (req, res) => {
-  res.status(200).send({ output: `Rota GET` });
+  Cliente.find()
+    .then((dados) => {
+      res.status(200).send({ output: dados });
+    })
+    .catch((erro) =>
+      res.status(500).send({
+        output: `Erro interno 
+        ao processar a consulta -> ${erro}`,
+      })
+    );
 });
 
 //rota para cadastrar os clientes com endpoint cadastrar
@@ -72,12 +87,29 @@ app.post(`${default_route}/cadastrar`, (req, res) => {
 //rota para atualizar os clientes com endpoint atualizar
 //passagem de argumentos pela url com o id do cliente
 app.put(`${default_route}/atualizar/:id`, (req, res) => {
-  res.status(200).send({ output: req.params.id });
+  Cliente.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (erro, dados) => {
+      if (erro) {
+        return res.status(500).send({ output: `Não atualizou -> ${erro}` });
+      }
+      res.status(200).send({ output: "Dados atualizados" });
+    }
+  );
 });
 
 //rota para apagar cliente com endpoint deletar
 app.delete(`${default_route}/apagar/:id`, (req, res) => {
-  res.status(204).send({ output: req.params.id });
+  Cliente.findByIdAndDelete(req.params.id, (erro, dados) => {
+    if (erro) {
+      return res
+        .status(500)
+        .send({ output: `Erro ao tentar apagar -> ${erro}` });
+    }
+    res.status(204).send({ output: `Apagar` });
+  });
 });
 
 //definir a porta de comunicação do servidor
